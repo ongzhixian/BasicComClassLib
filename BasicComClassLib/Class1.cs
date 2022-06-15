41e7685cc2bf4270f244c61e0aed966e7d1903b7
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-06-15 17:59:20 +0800

    init commit

---
+++ BasicComClassLib/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BasicComClassLib
+{
+
+
+    [Guid("FB70BBC7-D3DF-4B2B-BFFA-05A4E17F9D87")]
+    public interface BasicComClass1Interface
+    {
+    }
+
+    [Guid("BA9ECC61-B933-4F54-A405-02FDD7AB23E0"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface BasicComClass1Events
+    {
+    }
+
+    [Guid("463B59F1-A1BC-4FED-BD41-460163D185D5"), ClassInterface(ClassInterfaceType.None), ComSourceInterfaces(typeof(BasicComClass1Events))]
+    public class ComClass1 : BasicComClass1Interface
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BasicComClassLib
+
 A basic Component Object Model (COM) class library
+
+
+Using .NET Standard 2.0 to create a basic COM class library.
